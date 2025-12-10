28a6dd1d049d4a9bc4b61b830c55eee1d09a6d63
calizayaalex5 <calizayaalex5@gmail.com>
2025-12-10 12:06:40 -0300

    vite

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite"
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: "index.html",
+                catalog: "catalog.html",
+                product: "product.html",
+                cart: "cart.html",
+                diy: "diy.html",
+                support: "support.html",
+                checkout: "checkout.html",
+                success: "success.html",
+            },
+        },
+    },
+});
